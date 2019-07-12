b16bd170c7240af892071e6e164d4c7fbe308f88
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2019-07-12 18:41:31 +0000

    Initial module look

---
+++ Assets/luckyDiceScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KModkit;
+
+public class luckyDiceScript : MonoBehaviour 
+{
+	public KMBombInfo bomb;
+	public KMAudio Audio;
+
+	//Logging
+	static int moduleIdCounter = 1;
+    int moduleId;
+    private bool moduleSolved;
+
+	void Awake()
+	{
+		moduleId = moduleIdCounter++;
+		//btn.OnInteract += delegate () { PressButton(); return false; };
+	}
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
